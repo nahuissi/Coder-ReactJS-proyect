f4c7ebf2b9707e53568ced1d8fe34aa2dfc0fc71
nahuissi <nahuesteban2004@gmail.com>
2023-11-28 22:12:10 -0300

    mas commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
@@ -9,6 +10,23 @@ function App() {
       <ItemListContainer greeting="Tienda De Ropa En Descuento" />
       
     </div>
+=======
+import Home from "./componets/Home/Home";
+import CartContent from "./componets/CartContent/CartContent";
+import DataProvider from "./componets/Context/DataContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<CartContent />} />
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
+>>>>>>> 8c61840 (segundo commit)
   );
 }
 
---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className='banner'>
+      <div className='banner-container'>
+        <h1>Tienda de libros</h1>
+        <p>Encuentra aqui tus libros favoritos</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
